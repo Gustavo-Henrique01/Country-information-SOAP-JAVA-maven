8a53f42d29e6f2de19eeb9fdca85fa6faaab4a51
Gustavo-Henrique01 <gusta.2019.henrique@gmail.com>
2024-12-03 16:10:42 -0400

    criação da conexão com o banco do servico

--- src/main/java/br/edu/ifms/country_information/App.java
+++ src/main/java/br/edu/ifms/country_information/App.java
@@ -1,10 +1,34 @@
 package br.edu.ifms.country_information;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+
+import br.edu.ifms.country_information.servico.CountryInfoServiceSoapType;
+import jakarta.xml.ws.Service;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws MalformedURLException {
+    	
+    	
+    	URL urlCountryInformation = new URL("http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDl");
+        
+        QName qnameCountryInformation = new QName("http://www.oorsprong.org/websamples.countryinfo", "CountryInfoService");
+  
+        Service serviceContry = Service.create(urlCountryInformation, qnameCountryInformation);
+        
+        CountryInfoServiceSoapType  servico = serviceContry.getPort(CountryInfoServiceSoapType.class);
+        
+        
+        
+        
+            
+      
+      
+
     }
 }
